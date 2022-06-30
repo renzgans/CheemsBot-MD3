d5b079fe4ed4e16ff378f624dc2fd14053623406
renzgans <108325453+renzgans@users.noreply.github.com>
2022-06-30 12:33:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,23 +41,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.owner = ['6281250365581'] //ur owner number
+global.ownername = "Renz Store DL"
+global.ytname = "YT: Renz GTPS" //ur yt chanel name
+global.socialm = "GitHub: renzgans" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
+global.botname = "RenzBotz" //ur bot name
+global.websitex = "https://www.youtube.com/channel/UCaldPN9vH-LNBrBlP1efQKg" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.packname = "Renz Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
+global.author = "🦄RenzStore DL"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
+global.ownertag = ['6281250365581'] //ur owner tag
+global.ownernummenu = ['6281250365581'] //ur owner number in menu and all
+global.watermark = "Renz Bot Inc." //ur watermark
 global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
 global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
 global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
